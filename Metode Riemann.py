72e89c34710a6aefaa7e6ae8bb0ae0e806f61464
dianiwati <114728139+dianiwati@users.noreply.github.com>
2022-12-22 22:11:35 +0700

    Add files via upload

---
+++ Metode Riemann.py
@@ -0,0 +1,25 @@
+#Metode Reimann
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def func(x):
+    return (x**2)*p.exp(-x)
+
+a = 1.0
+b = 10.0
+n = 1000
+x = np.linspace (a, b,n) 
+dx = (x[-1]-x[0])/(n-1)
+
+hasil = 0
+
+for i in range (n-1) :
+  hasil += dx*func(x[i])
+
+xp = p.linspace (a, b)
+plt.plot (xp, func(xp))
+for i in range (n-1) :
+  plt.bar(x[i],func(x[i]), align = 'edge', width=dx, color = 'gray', edgecolor = 'red')
+plt.show()
+print (hasil)
